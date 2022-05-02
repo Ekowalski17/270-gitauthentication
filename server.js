d472a6d312d3f62faf1f4c2522219da5acec7c04
Elizabeth Davis <elizabethdavis@Elizabeths-MacBook-Pro.local>
2022-05-02 13:45:36 -0600

    insert your message here

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');//importing the library
-
+const port = 3000
 const app = express();//using the library
 
-app.listen(3000,()=>{console.log("listening.....")}); //listen
+app.listen(port,()=>{console.log("listening....." + port)}); //listen
 
 app.get('/', (request, response)=>{response.send("hello")}); //respond
