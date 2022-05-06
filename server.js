4a7dd3e42ef2a347e0cce5b9df4ffdfb5d94d832
Elizabeth Davis <elizabethdavis@Elizabeths-MacBook-Pro.local>
2022-05-06 13:33:16 -0600

    today's assignment

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-const express = require('express');//importing the library
-const port = 3000
-const app = express();//using the library
+const express = require('express'); //import the express library
+const bodyparser = require('body-parser'); //import body-parser
+const app = express(); //using express library to get application module
+app.use(bodyparser.json());//add body parser middleware
+const port = 3000;
+//listen
+app.listen(port, ()=>{
+    console.log("Listening on port: "+port)
+});
 
-app.listen(port,()=>{console.log("listening....." + port)}); //listen
 
-app.get('/', (request, response)=>{response.send("hello")}); //respond
+//validate password function
+const  validatePassword = (request,response) => {
+    if(request.body.userName == "cupcakes123@gmail.com" && request.body.password == "Cupcakesarebadforyou_22$"){
+        response.status(200);
+        response.send("Welcome")
+    } else {
+        response.status(401);
+        response.send("Invalid userName or password");
+    }
+}
+//respond
+app.get('/',(request,response)=>{response.send("hello")});
+app.post('/login',validatePassword);
+
+
+
+
+
+
+
+
+
